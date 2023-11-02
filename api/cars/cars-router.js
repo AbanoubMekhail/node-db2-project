4effa7384b9c28fe11b7a1a5b92cef1bcda51e05
abanoub nagib <abanoub.nagib@hotmail.com>
2023-11-02 05:31:07 -0700

    completed

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,34 @@
 // DO YOUR MAGIC
+const express = require('express')
+const Car = require('./cars-model')
+const carsMid = require('./cars-middleware')
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id', carsMid.checkCarId, async (req, res, next) => { // eslint-disable-line
+   res.json(req.car)
+})
+
+router.post(
+    '/',
+    carsMid.checkCarPayload,
+    carsMid.checkVinNumberUnique,
+    carsMid.checkVinNumberValid,
+async (req, res, next) => { // eslint-disable-line
+    try {
+        const car = await Car.create(req.body)
+        res.json(car)
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+    {
+        vin: "1FAFP404XXF1234567",
+        make: "toyota",
+        model: "prius",
+        mileage: 215000,
+        title: "clean",
+        transmission: "manual",
+    },
+    {
+        vin: "2G1WF55E6591234567",
+        make: "toyota",
+        model: "corolla",
+        mileage: 115000,
+        title: "salvage",
+    },
+    {
+        vin: "JTDBE32K3201234567",
+        make: "ford",
+        model: "focus",
+        mileage: 15000,
+    },
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
